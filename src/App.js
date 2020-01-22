2554ee510c561fa2705e891c739cba8dd8d2ed60
Cody Young <codyjamesyoung@gmail.com>
2020-01-22 00:34:20 -0700

    functional create post and delete all front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      title: "",
+      input: "",
+      post: {}
+    };
+
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  handleAddTask() {
+    this.setState({
+      list: [
+        ...this.state.list,
+        {
+          post: {
+            title: this.state.title,
+            content: this.state.input,
+            time: new Date().toLocaleTimeString([], {
+              timeStyle: "short",
+              dateStyle: "medium"
+            })
+          }
+        }
+      ],
+      input: "",
+      title: ""
+    });
+  }
+
+  handleTitleChange(value) {
+    this.setState({ title: value });
+  }
+
+  handleInputChange(value) {
+    this.setState({ input: value });
+  }
+
+  removePost(name) {
+    this.setState({
+      list: this.state.list.filter(el => el !== name)
+    });
+  }
+
+  render() {
+    let list = this.state.list.map((el, i) => {
+      console.log(el.post);
+      return (
+        <div className="post" key={i}>
+          <h3>{el.post.title} </h3>
+          <h4>{el.post.content}</h4>
+          <p>{el.post.time}</p>
+          <button className='delete'
+            onClick={() => {
+              this.removePost(el);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      );
+    });
+    return (
+      <div className="App">
+        <div className="form">
+        <h3>Add New Log</h3>
+          <input
+            value={this.state.title}
+            placeholder="Log Title"
+            onChange={e => this.handleTitleChange(e.target.value)}
+          />
+          <textarea
+            value={this.state.input}
+            placeholder="Log Message"
+            onChange={e => this.handleInputChange(e.target.value)}
+          />
+
+          <button onClick={this.handleAddTask}>Submit Log</button>
+        </div>
+        {list}
+      </div>
+    );
+  }
 }
 
 export default App;
